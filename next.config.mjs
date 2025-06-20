e1dd448d35c7d77d482d8c049fdf1c83cc2a9635
Marcell Alessandro Reynoso Flores <marcell.reynoso.flores@gmail.com>
2025-06-20 11:39:25 -0500

    Fix: conflict merge

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-=======
-  ...(process.env.NODE_ENV === 'production' && {
-    basePath: '/marcell',
-    assetPrefix: '/marcell',
-  }),
->>>>>>> 3c7b3f64763050fb31546be81c57b8b768ccca6f
 };
 
 export default nextConfig;
