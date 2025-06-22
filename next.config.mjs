a1340d48afbf5f6311bd51f886bc31cc52633ec0
Rooss <rooss.pacci@gmail.com>
2025-06-22 15:09:51 -0500

    Add: Configure basePath and assetPrefix for production environment in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    ...(process.env.NODE_ENV === 'production' && {
+        basePath: '/marcell',
+        assetPrefix: '/marcell',
+    }),
 };
-
 export default nextConfig;
