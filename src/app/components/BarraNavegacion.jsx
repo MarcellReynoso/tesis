2a00f760c3cdca2c10c51c3aa90afe0d546b807e
Rooss <rooss.pacci@gmail.com>
2025-06-22 16:02:15 -0500

    Update: Implement dynamic basePath for navigation links based on production environment

--- src/app/components/BarraNavegacion.jsx
+++ src/app/components/BarraNavegacion.jsx
@@ -7,11 +7,15 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import OptimizedImage from "./OptimizedImage";
 
+// Determinar si estamos en producción para usar la carpeta base
+const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? '/marcell' : '';
+
 const navigation = [
-  { name: "Dashboard general", href: `/datos` },
-  { name: "Ambiental", href: `/ambiental` },
-  { name: "Corporal", href: `/corporal`},
-  { name: "Sensores", href: `/sensores` },
+  { name: "Dashboard general", href: `${basePath}/datos` },
+  { name: "Ambiental", href: `${basePath}/ambiental` },
+  { name: "Corporal", href: `${basePath}/corporal`},
+  { name: "Sensores", href: `${basePath}/sensores` },
 ];
 
 export default function BarraNavegacion() {
@@ -36,7 +40,7 @@ export default function BarraNavegacion() {
           <div className="hidden md:flex flex-1 items-center justify-between">
             {/* Logo */}
             <div className="">
-              <Link href={"/"}>
+              <Link href={`${basePath}/`}>
                 <OptimizedImage 
                   alt="Logo" 
                   src="/img/logoUNJBGBlanco.png"
